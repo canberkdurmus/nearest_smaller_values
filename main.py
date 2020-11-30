811e567ebf7968f84dc16c41c3d92af4201e4c01
canberkdurmus <canberkdurmus@gmail.com>
2020-11-30 11:26:08 +0300

    Formatting with comments

--- main.py
+++ main.py
@@ -1,62 +1,48 @@
 # Data structure to store a Cartesian tree node
 class Node:
     def __init__(self, data, index=None, left=None, right=None):
+        # Content of the node
         self.data = data
-        self.left = left
+        # Right child of the node
         self.right = right
+        # Left child of the node
+        self.left = left
+        # Inherited value with the right traversal
         self.right_inherited = None
+        # Inherited value with the left traversal
         self.left_inherited = None
+        # Index of the node in the inorder array
         self.index = index
 
 
+# Variable is global because array is being filled by a recursive function
 nodes = []
 
 
-# Recursive function to perform inorder traversal of a Cartesian tree
-def inorder_traversal(current_root):
-    if current_root is None:
-        return
-
-    inorder_traversal(current_root.left)
-    # print(current_root.data, end=' ')
-    print(current_root.data, current_root.index)
-    inorder_traversal(current_root.right)
-
-
+# Traversing the Cartesian Tree to find left smaller value for each node
 def inorder_traversal_left(current_root, inherited):
     if current_root is None:
         return
 
     current_root.left_inherited = inherited
-    # nsv_left = current_root.left_inherited
-
-    # if nsv_left is None:
-    #     print('-', end=' ')
-    # else:
-    #     print(inorder_array[nsv_left], end=' ')
 
     inorder_traversal_left(current_root.left, current_root.left_inherited)
     inorder_traversal_left(current_root.right, current_root.index)
 
 
+# Traversing the Cartesian Tree to find right smaller value for each node
 def inorder_traversal_right(current_root, inherited):
     if current_root is None:
         return
 
     current_root.right_inherited = inherited
-    # nsv_right = current_root.right_inherited
-
-    # if nsv_right is None:
-    #     print('-', end=' ')
-    # else:
-    #     print(inorder_array[nsv_right], end=' ')
     nodes.append(current_root)
 
     inorder_traversal_right(current_root.left, current_root.index)
     inorder_traversal_right(current_root.right, current_root.right_inherited)
 
 
-# Function to find index of the minimum element in inorder[start, end]
+# Function to find the index of the minimum element in an array, to find root of the cartesian trees or subtrees
 def min_element_index(inorder, start, end):
     min_index = start
     for i in range(start + 1, end + 1):
@@ -66,44 +52,38 @@ def min_element_index(inorder, start, end):
     return min_index
 
 
-# Recursive function to construct a Cartesian tree from given
-# inorder sequence
+# Recursive function to construct the cartesian tree, builds tree as subtrees with starting and ending indices
 def construct_tree(inorder, start, end):
-    # base case
     if start > end:
         return None
 
-    # Find index of the minimum element in inorder[start, end]
     index = min_element_index(inorder, start, end)
-
-    # The minimum element in given range of inorder sequence becomes the root
     current_root = Node(inorder[index])
-    # print(index, start, end, '\t', inorder)
     current_root.index = index
 
-    # recursively construct the left subtree
     current_root.left = construct_tree(inorder, start, index - 1)
-
-    # recursively construct the right subtree
     current_root.right = construct_tree(inorder, index + 1, end)
 
-    # return current node
     return current_root
 
 
 if __name__ == '__main__':
-    # input sequence of numbers representing the in-order sequence
-    inorder_array = [9, 3, 7, 1, 8, 12, 10, 20, 15, 18, 5]
-    # inorder_array = [5, 10, 40, 30, 28]
+    # Name of the data file, integers must be divided by ', ' (comma and one whitespace)
+    filename = 'data.txt'
+    fileObject = open(filename, "r")
+    data = fileObject.read()
+    inorder_array = [int(i) for i in data.split(', ')]
+    print('Array: ', inorder_array)
 
-    # construct the Cartesian tree
     root = construct_tree(inorder_array, 0, len(inorder_array) - 1)
 
     inorder_traversal_left(root, None)
     inorder_traversal_right(root, None)
 
+    # Every node comes with it's smaller right and left nearest values but the sequence is unsorted so we sort them
     nodes.sort(key=lambda x: x.index, reverse=False)
 
+    # Indices for the result array
     left_indices = []
     right_indices = []
 
@@ -111,6 +91,7 @@ if __name__ == '__main__':
         left_indices.append(n.left_inherited)
         right_indices.append(n.right_inherited)
 
+    # Printing the Nearest Smaller Values
     print('Nearest Left Smaller Values: ', end=' ')
     for i in left_indices:
         if i is None:
